78d565970f482cb19070537f235f824e1979be4c
pdefd77 <pdefd77@gmail.com>
2025-03-13 19:50:57 +0900

    매커니즘을 위한 클래스 생성

--- Assets/Scripts/ShapeClass.cs
+++ Assets/Scripts/ShapeClass.cs
@@ -1,6 +1,28 @@
 using UnityEngine;
 
-public class ShapeClass
+public enum Shape
 {
-    
+    Triangle,
+    Square,
+    pentagon,
+    hexagon
+}
+
+public enum Color
+{
+    Red,
+    Green,
+    Blue
+}
+
+public class CerealClass
+{
+    public Shape shape;
+    public Color color;
+
+    public CerealClass(Shape shape, Color color)
+    {
+        this.shape = shape;
+        this.color = color;
+    }
 }
