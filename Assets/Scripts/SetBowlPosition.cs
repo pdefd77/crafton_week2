70dd6aef28aa26977dd16552ecf4657eb4821796
pdefd77 <pdefd77@gmail.com>
2025-03-13 17:24:55 +0900

    Initial commit

---
+++ Assets/Scripts/SetBowlPosition.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SetBowlPosition : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.localScale = new Vector2(2, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
